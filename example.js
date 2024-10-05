8ec1e125148b998188ce5f6bab7dcdd6c4032d6c
Tahir Asadli <tahir-asadov@outlook.com>
2024-10-05 09:17:35 +0400

    Update

--- example.js
+++ example.js
@@ -1,26 +1,28 @@
+import Uploader from '@tahir-asadli/uploader';
+
 const requestHeaders = {};
 
 let csrfTokenMeta = document.querySelector("meta[name=\"csrf-token\"]");
 
-if(csrfTokenMeta != undefined) {
+if (csrfTokenMeta != undefined) {
   requestHeaders['X-CSRF-TOKEN'] = csrfTokenMeta.getAttribute("content");
 }
 
 const mediaConfig = {
-  
+
   fileInputId: 'file',
   url: '/upload',
   headers: requestHeaders,
-  started: function(fileList) {
+  started: function (fileList) {
     console.log('Upload started!');
   },
-  progress: function(fileId, progress) {
+  progress: function (fileId, progress) {
     console.log(`Uploading... ${progress}%`, fileId);
   },
-  completed: function(fileId) {
+  completed: function (fileId) {
     console.log(`File uploaded! ${fileId}`);
   },
-  error: function(fileId, error) {
+  error: function (fileId, error) {
     console.error(`${error} ${fileId}`);
   },
 }
